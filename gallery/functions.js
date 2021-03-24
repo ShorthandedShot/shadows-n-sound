758965bcf9c960b6a699d7e8e9a2fe3555d2861c
Jayce Nguyen <32528654+ShorthandedShot@users.noreply.github.com>
2021-03-24 14:39:20 -0400

    hmmmmmm

---
+++ gallery/functions.js
@@ -0,0 +1,15 @@
+function toggleSound() {
+  var audioElem = document.getElementById('audio');
+  if (audioElem.paused)
+    audioElem.play();
+  else
+    audioElem.pause();
+}
+
+function toggleSound() {
+  var audioElem = document.getElementById('audio1');
+  if (audioElem.paused)
+    audioElem.play();
+  else
+    audioElem.pause();
+}
